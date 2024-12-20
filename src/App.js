9ad704ecc412c1c3323e2a73df6f185f870faea3
Kky-01 <kim010425@naver.com>
2024-12-20 23:37:56 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Page46 from "./components/Page46";
 import Page47 from "./components/Page47";
-
+import Page48 from "./components/Page48";
+import Page49 from "./components/Page49";
+import Page50 from "./components/Page50";
+import Page51 from "./components/Page51";
+import Page52 from "./components/Page52";
 const Tab = createBottomTabNavigator();
 
 const App = () => (
@@ -24,11 +28,11 @@ const App = () => (
     >
       <Tab.Screen name="p.46" component={Page46} />
       <Tab.Screen name="p.47" component={Page47} />
-      <Tab.Screen name="p.48" component={Page46} />
-      <Tab.Screen name="p.49" component={Page46} />
-      <Tab.Screen name="p.50" component={Page46} />
-      <Tab.Screen name="p.51" component={Page46} />
-      <Tab.Screen name="p.52" component={Page46} />
+      <Tab.Screen name="p.48" component={Page48} />
+      <Tab.Screen name="p.49" component={Page49} />
+      <Tab.Screen name="p.50" component={Page50} />
+      <Tab.Screen name="p.51" component={Page51} />
+      <Tab.Screen name="p.52" component={Page52} />
       <Tab.Screen name="Diary" component={Page46} />
     </Tab.Navigator>
   </NavigationContainer>
