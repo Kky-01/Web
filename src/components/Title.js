db942b50327fbc983809dcd9724b55e8eebbf470
Kky-01 <kim010425@naver.com>
2024-12-21 07:31:02 +0900

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,10 +4,10 @@ import { View, Text, StyleSheet } from 'react-native';
 const Title = () => {
   return (
     <View style={styles.container}>
-      {/* 앱 이름 */}
+      {}
       <Text style={styles.title}>Diabetes care</Text>
 
-      {/* 앱 설명 */}
+      {}
       <Text style={styles.subtitle}>함께 하는 쉬운 건강 관리</Text>
     </View>
   );
@@ -15,18 +15,18 @@ const Title = () => {
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center', // 텍스트를 가운데 정렬
-    marginBottom: 20, // 아래 컴포넌트와 간격
+    alignItems: 'center', 
+    marginBottom: 20, 
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     color: 'red',
-    marginBottom: 5, // 앱 이름과 설명 텍스트 사이 간격
+    marginBottom: 5, 
   },
   subtitle: {
     fontSize: 16,
-    color: '#666', // 설명 텍스트 색상 (연한 회색)
+    color: '#666',
   },
 });
 
